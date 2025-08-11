93469cf5d1bc03d65e9b239aadfe663afa694e75
Ihor Dmytriv <dmytriwihor@gmail.com>
2025-08-11 10:02:31 +0200

    Add validation for email and password fields

--- src/schemas/accounts.py
+++ src/schemas/accounts.py
@@ -6,9 +6,19 @@ from database import accounts_validators
 class UserBaseSchema(BaseModel):
     email: EmailStr
 
+    @field_validator("email", mode="before")
+    @classmethod
+    def validate_email(cls, value: str) -> str:
+        return accounts_validators.validate_email(user_email=value)
+
 
 class UserRegistrationRequestSchema(UserBaseSchema):
     password: str
+
+    @field_validator("password")
+    @classmethod
+    def validate_password(cls, value: str) -> str:
+        return accounts_validators.validate_password_strength(password=value)
 
 
 class UserRegistrationResponseSchema(UserBaseSchema):
